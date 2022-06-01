de9d9f965ea2b0608d6d7bf64db0a40853aff0f5
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-06-01 21:07:18 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,20 +2,21 @@ const startBtn = document.querySelector("[data-start]");
 const stopBtn = document.querySelector("[data-stop]");
 const body = document.querySelector("body");
 startBtn.addEventListener("click", changeColor);
+let timerId = null;
 function changeColor(event) {
     function changeBodyColor() {
         body.style.backgroundColor = randomColor();
     };
-    setInterval(changeBodyColor, 1000);
+    timerId = setInterval(changeBodyColor, 1000);
+    startBtn.disabled = true;
 };
 stopBtn.addEventListener("click", stopChanging);
 function stopChanging() {
-    clearInterval(changeBodyColor);    
+    clearInterval(timerId); 
+    startBtn.disabled = false;
 };
 
-
-
-   const randomColor = function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    };
+const randomColor = function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
